b069d79dc24ffb0986e22e7394d13cffced81e06
SeongEun Park <77668478+bbung95@users.noreply.github.com>
2025-02-03 22:24:40 +0900

    #14 Icon Component add & storybook add (#15)
    * feat: svgr package add
    
    * feat: Icon component add & storybook add

---
+++ src/components/common/Icon/lib/index.ts
@@ -0,0 +1,11 @@
+/**
+ * Icon 정의
+ *
+ * TODO
+ * color 관련 작업 필요 fill, stroke, color
+ */
+
+export { default as home } from './home_icon.svg';
+export { default as mailbox } from './mailbox_icon.svg';
+export { default as message } from './message_icon.svg';
+export { default as setting } from './setting_icon.svg';
