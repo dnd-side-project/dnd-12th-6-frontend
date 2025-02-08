71dcc6a2b255224f2542104f283a7ea9ab2e99d3
김정우 <jwo0o0358@gmail.com>
2025-02-08 22:05:54 +0900

    fix: BottomNav에 background color 적용

--- src/components/layout/BottomNav/ui/BottomNavMain.tsx
+++ src/components/layout/BottomNav/ui/BottomNavMain.tsx
@@ -6,7 +6,7 @@ const BottomNavMain = ({ children }: BottomNavProps) => {
   return (
     <nav
       className='fixed bottom-0 w-full max-w-content h-64 px-5 pt-4
-      border-t border-gray-2 shadow-[0px_-1px_6px_rgba(0,0,0,0.05)] flex'
+      border-t border-gray-2 shadow-[0px_-1px_6px_rgba(0,0,0,0.05)] flex bg-white'
     >
       {children}
     </nav>
