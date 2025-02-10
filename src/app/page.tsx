ae23b91f975fe8ad181e46f1516f7560499c953b
김정우 <jwo0o0358@gmail.com>
2025-02-10 17:22:42 +0900

    chore: 메인 화면에 카드 모듈 UI 예시 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
+import CardScroller from '@/components/common/Card/layout/CardScroller';
+import CardDataFetcher from '@/components/common/Card/modules/CardDataFetcher';
+
 export default function Home() {
-  return <div>Main</div>;
+  return (
+    <div>
+      Main
+      <CardDataFetcher>{(cards) => <CardScroller cards={cards} />}</CardDataFetcher>
+    </div>
+  );
 }
