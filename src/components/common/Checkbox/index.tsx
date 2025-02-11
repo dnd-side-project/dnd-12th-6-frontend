ef5e549c428cea1adfe363cf00ae7261f4a86eec
bbung <tjd123123@naver.com>
2025-02-11 23:24:44 +0900

    docs: checkbox 주석추가

--- src/components/common/Checkbox/index.tsx
+++ src/components/common/Checkbox/index.tsx
@@ -6,6 +6,12 @@ import * as React from 'react';
 
 import { cn } from '@/lib/utils';
 
+/**
+ * Props
+ * - value : check시 값
+ * - checked : checkbox 체크
+ * - onCheckedChange : checkbox checked시 true | false 리턴
+ */
 const Checkbox = React.forwardRef<
   React.ElementRef<typeof CheckboxPrimitive.Root>,
   React.ComponentPropsWithoutRef<typeof CheckboxPrimitive.Root>
