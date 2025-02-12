43ae0725145b68cc239fcd465f609bf19fad548f
김정우 <jwo0o0358@gmail.com>
2025-02-12 15:38:23 +0900

    fix: tailwind text style 변수명 변경 반영

--- src/components/layout/Header/index.tsx
+++ src/components/layout/Header/index.tsx
@@ -1,6 +1,6 @@
 const Header = () => {
   return (
-    <div className='fixed top-0 w-full max-w-content h-[54px] px-[22px] py-10 bg-white text-primary-neon text-title'>
+    <div className='fixed top-0 w-full max-w-content h-[54px] px-[22px] py-10 bg-white text-primary-neon typo-title'>
       Invity
     </div>
   );
