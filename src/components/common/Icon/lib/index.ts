ae430d11d9436708a8f67e97791c0f6e1263a81a
SeongEun Park <77668478+bbung95@users.noreply.github.com>
2025-02-13 23:28:46 +0900

    Refactor/Input 컴포넌트 스타일 수정 & NumberInput 추가 (#33)
    * refactor: field 스타일 수정
    
    * refactor: input 스타일 수정
    
    * feat: input apply case 추가
    
    * feat: number input add
    
    * feat: label optional add

--- src/components/common/Icon/lib/index.ts
+++ src/components/common/Icon/lib/index.ts
@@ -5,8 +5,11 @@
  * color 관련 작업 필요 fill, stroke, color
  */
 
+export { default as check_circle } from './check_circle_icon.svg';
 export { default as error } from './error_icon.svg';
 export { default as home } from './home_icon.svg';
 export { default as mailbox } from './mailbox_icon.svg';
 export { default as message } from './message_icon.svg';
+export { default as minus } from './minus_icon.svg';
+export { default as plus } from './plus_icon.svg';
 export { default as setting } from './setting_icon.svg';
--- src/components/common/Input/Input.stories.tsx
+++ src/components/common/Input/Input.stories.tsx
@@ -1,26 +1,70 @@
 import type { Meta, StoryObj } from '@storybook/react';
+import { ChangeEvent, useRef, useState } from 'react';
 
-import Input from '.';
+import InputRoot from '.';
+import Icon from '../Icon';
+import NumberInput from '../NumberInput';
 
-const meta: Meta<typeof Input> = {
+const meta: Meta<typeof InputRoot> = {
   title: 'Common/Input',
-  component: Input,
+  component: InputRoot,
   parameters: {
     layout: 'centered',
   },
   tags: ['autodocs'],
   argTypes: {},
-  args: {
-    placeholder: '플레이스 홀더',
-  },
-} satisfies Meta<typeof Input>;
+  args: {},
+} satisfies Meta<typeof InputRoot>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
+  args: {
+    children: (
+      <>
+        <InputRoot.Input placeholder='플레이스 홀더' />
+      </>
+    ),
+  },
+};
+
+export const Apply: Story = {
   args: {},
-  render: (args) => {
-    return <Input {...args} />;
+  render: () => {
+    const [text, setText] = useState('입력 값');
+
+    const handleChangeText = (e: ChangeEvent<HTMLInputElement>) => {
+      const value = e.target.value;
+
+      setText(value);
+    };
+
+    return (
+      <InputRoot>
+        <InputRoot.Input value={text} onChange={handleChangeText} />
+        {text && <Icon name='check_circle' className='w-[22px] h-[22px] text-primary-strong' />}
+      </InputRoot>
+    );
+  },
+};
+
+export const NumberType: Story = {
+  args: {},
+  render: () => {
+    const inputRef = useRef<HTMLInputElement>(null);
+
+    return (
+      <div className='min-w-30'>
+        <NumberInput ref={inputRef} min={1} max={20} />
+        <button
+          onClick={() => {
+            alert(inputRef.current?.value);
+          }}
+        >
+          check
+        </button>
+      </div>
+    );
   },
 };
