21058e5b7a0c48ce41079c642bc33320f9888d05
bbung <tjd123123@naver.com>
2025-02-16 03:24:48 +0900

    feat: bottom-sheet ui add

--- src/components/common/DateInput/index.tsx
+++ src/components/common/DateInput/index.tsx
@@ -1,5 +1,15 @@
 import React from 'react';
 
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from '../BottomSheet';
+import Field from '../Field';
 import Icon from '../Icon';
 import InputRoot from '../Input';
 
@@ -10,11 +20,36 @@ interface Props {
 }
 
 const DateInput = ({ ...rest }: Props) => {
+  const side = 'bottom';
+
   return (
-    <div className='px-16 py-[14px] flex items-center  border-[1.2px] border-gray-2 rounded-sm'>
-      <InputRoot.Input {...rest} readOnly />{' '}
-      <Icon name='calendar' className='w-[21px] h-[21px] text-gray-5' />
-    </div>
+    <Sheet key={side}>
+      <SheetTrigger asChild>
+        <div className='cursor-pointer px-16 py-[14px] flex items-center  border-[1.2px] border-gray-2 rounded-sm'>
+          <InputRoot.Input {...rest} readOnly />{' '}
+          <Icon name='calendar' className='w-[21px] h-[21px] text-gray-5' />
+        </div>
+      </SheetTrigger>
+      {/* 바텀시트 영역 */}
+      <SheetContent side={side}>
+        <SheetHeader>
+          <SheetTitle>일시 선택하기</SheetTitle>
+        </SheetHeader>
+        <hr className='w-full h-2 bg-primary-strong' />
+        <div className='pt-32 px-20 pb-[50px]'>
+          <Field>
+            <Field.Label>날짜</Field.Label>
+            <div className='cursor-pointer px-16 py-[14px] flex items-center  border-[1.2px] border-gray-2 rounded-sm'>
+              <InputRoot.Input {...rest} readOnly />{' '}
+              <Icon name='calendar' className='w-[21px] h-[21px] text-gray-5' />
+            </div>
+          </Field>
+        </div>
+        <SheetFooter>
+          <SheetClose asChild>{/* <Button type='submit'>Save changes</Button> */}</SheetClose>
+        </SheetFooter>
+      </SheetContent>
+    </Sheet>
   );
 };
 
