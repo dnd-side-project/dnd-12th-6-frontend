0d8b808c07a6450e92069b4dd7b7480540a82a2e
김정우 <jwo0o0358@gmail.com>
2025-02-18 18:40:29 +0900

    feat: 초대장 꾸미기 초기화 버튼 구현

---
+++ src/components/card/ResetButton.tsx
@@ -0,0 +1,37 @@
+'use client';
+
+import clsx from 'clsx';
+
+import { useInvitationStore } from '@/store/invitationStore';
+
+import Icon from '../common/Icon';
+
+const ResetButton = () => {
+  const { invitation, isModified, resetInvitation } = useInvitationStore();
+  const isActive = isModified && invitation.title !== '';
+
+  return (
+    <div className='absolute top-16 right-16 flex flex-col gap-y-[5px]'>
+      <button
+        className='flex items-center justify-center w-[42px] h-[42px] bg-white rounded-full shadow-[0_0_4px_0_rgba(0,0,0,0.15)]'
+        onClick={isActive ? resetInvitation : undefined}
+        disabled={!isActive}
+      >
+        <Icon
+          name='back'
+          className={clsx('w-[22px] h-[22px]', isActive ? 'text-primary-strong' : 'text-gray-3')}
+        />
+      </button>
+      <div
+        className={clsx(
+          'w-[42px] typo-caption1 font-medium text-center',
+          isActive ? 'text-white' : 'text-gray-3',
+        )}
+      >
+        초기화
+      </div>
+    </div>
+  );
+};
+
+export default ResetButton;
