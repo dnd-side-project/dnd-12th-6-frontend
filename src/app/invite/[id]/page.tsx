69ab46bf051a8284bf7cf27754c72209fbbc2d88
bbung <tjd123123@naver.com>
2025-02-19 21:12:24 +0900

    feat: footer area add

--- src/app/invite/[id]/page.tsx
+++ src/app/invite/[id]/page.tsx
@@ -3,6 +3,7 @@ import PageContainer from '@/components/layout/PageContainer';
 import BackgroundView from './components/BackgroundView';
 import CTAButton from './components/CTAButton';
 import CardViewArea from './components/CardViewArea';
+import FooterArea from './components/FooterArea';
 import InviteContentArea from './components/InviteContentArea';
 
 const page = () => {
@@ -11,6 +12,7 @@ const page = () => {
       <BackgroundView />
       <CardViewArea />
       <InviteContentArea />
+      <FooterArea />
       <CTAButton />
     </PageContainer>
   );
