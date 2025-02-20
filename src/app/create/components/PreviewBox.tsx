db407b4daf8d77cd6b6551a517596a7c4572cae8
김정우 <jwo0o0358@gmail.com>
2025-02-20 18:21:08 +0900

    fix: 초대장 만들기 문구 '우리만의 초대장 꾸미기' text-nowrap 추가

--- src/app/create/components/PreviewBox.tsx
+++ src/app/create/components/PreviewBox.tsx
@@ -15,7 +15,7 @@ const PreviewBox = () => {
       <div className='w-[190px] absolute top-80 left-1/2 -translate-x-1/2 flex flex-col items-center gap-24'>
         <Image src='/images/create/letter_image.png' width={106} height={130} alt='' />
         <p className='typo-label1 text-primary-strong'>
-          여기를 눌러 <b className='font-bold'>우리만의 초대장 꾸미기</b>
+          여기를 눌러 <b className='font-bold text-nowrap'>우리만의 초대장 꾸미기</b>
         </p>
       </div>
       <div className='w-[157px] h-[44px] absolute bottom-[-22px] left-1/2 -translate-x-1/2 flex items-center justify-center border border-primary-normal rounded-xl bg-primary-natural'>
