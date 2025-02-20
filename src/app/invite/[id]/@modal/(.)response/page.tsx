3f816b1197f5d2fecbe08d93c33aa0bd18e0f2b4
bbung <tjd123123@naver.com>
2025-02-20 16:04:15 +0900

    refactor: 응답하기 페이지 모달 index 수정

--- src/app/invite/[id]/@modal/(.)response/page.tsx
+++ src/app/invite/[id]/@modal/(.)response/page.tsx
@@ -1,15 +1,26 @@
 'use client';
 
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import { PageHeaderCloseButton } from '@/components/invite/PageHeaderButton';
 import ResponseForm from '@/components/invite/ResponseForm';
 import PageContainer from '@/components/layout/PageContainer';
+import { ROUTER_TITLE } from '@/constants/enum';
 
 const page = () => {
+  useEffect(() => {
+    document.body.classList.add('overflow-hidden');
+    return () => {
+      document.body.classList.remove('overflow-hidden');
+    };
+  }, []);
+
   return (
-    <div className='min-h-dvh  absolute top-0 bottom-0 left-0 right-0 bg-white z-100'>
-      <PageContainer.PageHeader title='응답하기' right={<PageHeaderCloseButton />} />
+    <div className='absolute top-0 bottom-0 left-0 right-0 bg-white z-50 overflow-y-scroll'>
+      <PageContainer.PageHeader
+        title={ROUTER_TITLE.INVITE_RESPONSE}
+        right={<PageHeaderCloseButton />}
+      />
       <div className='min-h-dvh py-32 px-[18px]'>
         <ResponseForm />
       </div>
--- src/constants/enum.ts
+++ src/constants/enum.ts
@@ -1,4 +1,5 @@
 export const ROUTER_TITLE = {
   CREATE: '초대장 만들기',
   EDIT_CARD: '초대장 꾸미기',
+  INVITE_RESPONSE: '응답하기',
 } as const;
