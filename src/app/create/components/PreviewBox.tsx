81b8d8d561636238525b417f5e56747b2e8d2b1e
bbung <tjd123123@naver.com>
2025-02-21 18:23:52 +0900

    feat: 초대장 생성 카드 선택한 카드 프리뷰 추가

--- src/app/create/components/PreviewBox.tsx
+++ src/app/create/components/PreviewBox.tsx
@@ -1,10 +1,56 @@
 'use client';
 
 import Image from 'next/image';
-import { useRouter } from 'next/navigation';
+import { usePathname, useRouter } from 'next/navigation';
 import React from 'react';
 
+import CardDisplay from '@/components/card/CardDisplay';
+import { IInvitation } from '@/lib/invitation/types';
+import { useInvitationStore } from '@/store/invitationStore';
+
 const PreviewBox = () => {
+  const pathName = usePathname();
+
+  const { invitation } = useInvitationStore();
+
+  return (
+    <>
+      {!pathName.includes('create/card') && invitation.title ? (
+        // 카드가 있는 경우
+        <PreviewCard {...invitation} />
+      ) : (
+        <EmptyCard />
+      )}
+    </>
+  );
+};
+
+export default PreviewBox;
+
+const PreviewCard = (invitation: IInvitation) => {
+  const router = useRouter();
+
+  return (
+    <div
+      className='cursor-pointer w-full relative h-[360px]'
+      style={{
+        backgroundImage: invitation.background
+          ? `url(/images/background/background_${invitation.background}.png)`
+          : `url(${invitation.backgroundImageData})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+      }}
+      onClick={() => router.push('/create/card')}
+    >
+      <div className='absolute left-1/2 -translate-x-1/2'>
+        <CardDisplay {...invitation} />
+      </div>
+    </div>
+  );
+};
+
+// 카드가 없는 경우
+const EmptyCard = () => {
   const router = useRouter();
 
   return (
@@ -18,11 +64,10 @@ const PreviewBox = () => {
           여기를 눌러 <b className='font-bold'>우리만의 초대장 꾸미기</b>
         </p>
       </div>
+
       <div className='w-[157px] h-[44px] absolute bottom-[-22px] left-1/2 -translate-x-1/2 flex items-center justify-center border border-primary-normal rounded-xl bg-primary-natural'>
         <p className='typo-body font-bold text-primary-strong'>꾸미기</p>
       </div>
     </div>
   );
 };
-
-export default PreviewBox;
