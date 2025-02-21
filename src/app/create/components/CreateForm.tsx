de9b91dd9821e8ba9f3408636cfcd8d1abd6bcfb
bbung <tjd123123@naver.com>
2025-02-21 16:17:38 +0900

    refactor: 초대장 생성 formik -> react-hook-form 변경

--- src/app/create/components/CreateForm.tsx
+++ src/app/create/components/CreateForm.tsx
@@ -1,7 +1,8 @@
 'use client';
 
-import { useFormik } from 'formik';
+import { yupResolver } from '@hookform/resolvers/yup';
 import React from 'react';
+import { Resolver, useForm } from 'react-hook-form';
 import * as yup from 'yup';
 
 import DateInput from '@/components/common/DateInput';
@@ -11,12 +12,13 @@ import NumberInput from '@/components/common/NumberInput';
 import Textarea from '@/components/common/Textarea';
 
 interface SaveCreateFormDataType {
+  title: string;
   detail_address: string;
   date: string;
   max_attendances: number;
-  description: string;
-  title: string;
   invitationType: 'CREATOR';
+
+  description?: string;
   backgroundImageData?: string;
 
   invitationId?: number;
@@ -31,8 +33,23 @@ interface SaveCreateFormDataType {
 }
 
 const CreateForm = () => {
-  const formik = useFormik<SaveCreateFormDataType>({
-    initialValues: {
+  const validateSchema = yup.object().shape({
+    title: yup.string().min(1).max(10).required('이름을 입력해주세요.'),
+    detail_address: yup.string().required('장소명을 입력해주세요.'),
+    date: yup.string().required('모임 임시를 선택해주세요.'),
+    max_attendances: yup.number().min(1).max(20).required(),
+    description: yup.string().max(200),
+    invitationType: yup.string().oneOf(['CREATOR']),
+  });
+
+  const {
+    register,
+    watch,
+    setValue,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<SaveCreateFormDataType>({
+    defaultValues: {
       title: '',
       detail_address: '',
       date: '',
@@ -40,62 +57,42 @@ const CreateForm = () => {
       description: '',
       invitationType: 'CREATOR',
     },
-    validationSchema: yup.object({
-      title: yup.string().min(1).max(10).required('이름을 입력해주세요.'),
-      detail_address: yup.string().required('장소명을 입력해주세요.'),
-      date: yup.string().required('모임 임시를 선택해주세요.'),
-      max_attendances: yup.number().min(1).max(20),
-      description: yup.string().max(200),
-    }),
-    onSubmit: (values) => {
-      saveInvite(values);
-    },
+    resolver: yupResolver(validateSchema) as Resolver<SaveCreateFormDataType>,
   });
 
-  const saveInvite = async (values: SaveCreateFormDataType) => {
+  const submitInvite = async (values: SaveCreateFormDataType) => {
     console.log(values);
   };
 
   return (
-    <>
+    <form onSubmit={handleSubmit(submitInvite)}>
       <div className='py-40 px-[18px] flex flex-col gap-32'>
         <Field className='mb-[22px]'>
           <Field.Label required>주최자명</Field.Label>
           <InputRoot>
-            <InputRoot.Input
-              name='title'
-              placeholder='이름 또는 닉네임'
-              value={formik.values.title}
-              onChange={formik.handleChange}
-            />
+            <InputRoot.Input placeholder='이름 또는 닉네임' {...register('title')} />
           </InputRoot>
-          {formik.errors.title && (
-            <Field.HelpText status='error'>{formik.errors.title}</Field.HelpText>
-          )}
+          {errors.title && <Field.HelpText status='error'>{errors.title.message}</Field.HelpText>}
         </Field>
         <Field className='mb-[22px]'>
           <Field.Label required>장소</Field.Label>
           <InputRoot>
             <InputRoot.Input
-              name='address_detail'
               placeholder='ex) 강남역 7번 출구, 아파트 101동 102호'
-              value={formik.values.detail_address}
-              onChange={formik.handleChange}
+              {...register('detail_address')}
             />
           </InputRoot>
-          {formik.errors.detail_address && (
-            <Field.HelpText status='error'>{formik.errors.detail_address}</Field.HelpText>
+          {errors.detail_address && (
+            <Field.HelpText status='error'>{errors.detail_address.message}</Field.HelpText>
           )}
         </Field>
         <Field className='mb-[22px]'>
           <Field.Label required>일시</Field.Label>
           <DateInput
-            value={formik.values.date}
-            onChangeValue={(value: string) => formik.setFieldValue('date', value)}
+            value={watch('date')}
+            onChangeValue={(value: string) => setValue('date', value)}
           />
-          {formik.errors.date && (
-            <Field.HelpText status='error'>{formik.errors.date}</Field.HelpText>
-          )}
+          {errors.date && <Field.HelpText status='error'>{errors.date.message}</Field.HelpText>}
         </Field>
       </div>
 
@@ -105,18 +102,17 @@ const CreateForm = () => {
         <Field>
           <Field.Label optional>초대 인원</Field.Label>
           <NumberInput
-            value={formik.values.max_attendances}
-            onChangeValue={(value: number) => formik.setFieldValue('max_length', value)}
+            {...register('max_attendances')}
+            // value={formik.values.max_attendances}
+            // onChangeValue={(value: number) => formik.setFieldValue('max_length', value)}
           />
         </Field>
         <Field>
           <Field.Label optional>메세지</Field.Label>
           <Textarea
-            name='message'
-            value={formik.values.description}
-            onChange={formik.handleChange}
             placeholder='전달할 메세지를 입력해주세요.'
             counter
+            {...register('description')}
           />
         </Field>
       </div>
@@ -124,11 +120,10 @@ const CreateForm = () => {
       <button
         className='sticky bottom-0 w-full h-[57px] flex items-center justify-center bg-gray-7 disabled:bg-gray-3'
         type='submit'
-        onClick={() => formik.handleSubmit()}
       >
         <p className='typo-heading font-semibold text-white'>완료</p>
       </button>
-    </>
+    </form>
   );
 };
 
