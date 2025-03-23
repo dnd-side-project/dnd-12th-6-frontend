77acc57713b2f13c2bdce0b0c3723c1909da405d
김정우 <jwo0o0358@gmail.com>
2025-03-23 17:43:12 +0900

    fix: ResponseCompleteAlert 스토리북 수정

--- src/components/invite/ResponseCompleteAlert/ResponseCompleteAlert.stories.tsx
+++ src/components/invite/ResponseCompleteAlert/ResponseCompleteAlert.stories.tsx
@@ -1,12 +1,23 @@
+import { useArgs } from '@storybook/preview-api';
 import type { Meta, StoryObj } from '@storybook/react';
 
-import ResponseCompleteAlert from './ResponseCompleteAlert';
+import ResponseCompleteAlert from '.';
 
-const meta: Meta = {
+const meta: Meta<typeof ResponseCompleteAlert> = {
   title: 'Components/ResponseCompleteAlert',
   component: ResponseCompleteAlert,
-  argTypes: {},
-} satisfies Meta<typeof ResponseCompleteAlert>;
+  argTypes: {
+    completeType: {
+      control: { type: 'radio' },
+      options: ['response', 'edit'],
+      description: '응답 완료 유형',
+    },
+    open: {
+      control: { type: 'boolean' },
+      description: 'Drawer 열림 여부',
+    },
+  },
+};
 
 export default meta;
 
@@ -15,5 +26,19 @@ type Story = StoryObj<typeof meta>;
 export const Default: Story = {
   args: {
     completeType: 'response',
+    open: true,
+  },
+  render: (args) => {
+    const [, updateArgs] = useArgs();
+
+    return (
+      <ResponseCompleteAlert
+        {...args}
+        onClose={() => {
+          updateArgs({ open: false });
+          console.log('Drawer closed');
+        }}
+      />
+    );
   },
 };
